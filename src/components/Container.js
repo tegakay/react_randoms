9cf5cf56bcd1a7a5a6a3962354b267956762f303
Oghenebrume Kofi <kofitega@gmail.com>
2022-05-23 22:06:37 +0100

    squashed some bugs

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,55 +1,46 @@
 import React,{useState,useEffect} from "react";
 import Card from "./Card";
+import NoteList from "./NoteList";
 
 export default function Container(props){
     const [imdbList, setImdbList] = useState()
+    const [Loading,setLoading] = useState(true)
 
     var status = props.type;
     console.log("s",status)
 
-    
-        
+       
+          //fetch('https://imdb-api.com/en/API/InTheaters/k_3lc4097t')
         useEffect(()=>{
 
             
-            if(status ==="home"){
-                //fetch('https://imdb-api.com/en/API/InTheaters/k_3lc4097t')
-                fetch('https://api.imgflip.com/get_memes')
-                .then(res=>res.json())
-                .then(data=>setImdbList(data.data.memes))
-                 
-             //      return      
-            }
+            fetchreq();
 
         },[])
 
-    
+        const fetchreq = () =>{
+            if(status ==="home"){
+               
+                fetch('https://api.imgflip.com/get_memes')
+                .then(res=>res.json())
+                .then(data=>setImdbList(data.data.memes))
+                .then(data=>{return setLoading(false)})
+                
+            }
+        }
     
 
-
+        if (Loading) {
+            return <div className="App">Loading...</div>;
+          }
 
 
        console.log("stuff",imdbList)
 
-       const imdblistdata = imdbList.map(cards=>{
-        return(
-            <li>
-                 <Card
-            key = {cards.id}
-            name = {cards.name}
-            url = {cards.url}
-             />
-            </li>
-           
-        )
-
-       })
-  
-// {imdblistdata}
-    return(
-       <ul className="list-items">
-           
-           {imdblistdata}
-       </ul>
-    )
+    
+return(
+    <NoteList 
+    notes ={imdbList}/>
+)
+   
 }
---
+++ src/components/NoteList.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import Card from './Card';
+
+
+export default function NoteList(props) {
+    var imdbList = props.notes
+
+   
+
+    const imdblistdata = imdbList.map(cards=>{
+        if(imdbList.length >0){
+            return(
+                <li>
+                     <Card
+                key = {cards.id}
+                name = {cards.name}
+                url = {cards.url}
+                 />
+                </li>
+               
+            )
+        }
+        else{
+          return(<h1>No Data Yet</h1>)
+        }     
+
+       })
+  return (
+    <div>
+     <ul>
+         {imdblistdata}
+     </ul>
+    
+    </div>
+  );
+}
+
+
